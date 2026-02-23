de0977dae68e16365e6c34f711ba5e13009bab43
Tim Carney <tim.carney@cargt.com>
2026-02-23 08:49:39 -0600

    WIP: Add preliminary support for 5.5" MIPI-DSI Display GLT0557201280IS1

--- cargt-00395-00365v3/CA35/DeviceTree/cargt-00395-00365v3/optee-os/conf.mk
+++ cargt-00395-00365v3/CA35/DeviceTree/cargt-00395-00365v3/optee-os/conf.mk
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: BSD-2-Clause
 flavorlist-MP25 += stm32mp257f-cargt-00395-00365v3.dts \
+					stm32mp257f-cargt-00395-00365v3-glt0557201280is1.dts \
 					stm32mp257f-cargt-00395-00365v3-glt0701024600is2.dts \
 					stm32mp257f-cargt-00395-00365v3glt1011280800is1.dts
